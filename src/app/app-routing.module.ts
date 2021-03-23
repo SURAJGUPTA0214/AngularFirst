859160ed829f895dc6ae3bddf665ac031cbd2e77
SURAJGUPTA0214 <suraj.gupta.0214@gmail.com>
2021-03-23 15:06:31 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path:'postalook',Component=postalook},
+  // { path:'login', component:login },
+  // { path:'flaunt', component:flaunt },
+  // { path:'read', component:'read' },
+  // { path:'watch', component:'watch' },
+  // { path:'shop', component:'shop' },
+  // { path:'stylewedding', component:'stylewedding' },
+  // { path:'chrityauction', component:'chrityauction' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
